a34dd9c04fe90cdf5616aa10575b0e5506080d02
evgenii <vovk.evgenii.199811@gmail.com>
2025-05-22 16:41:11 +0300

    change

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from datetime import datetime
 
 
 class Logger:
@@ -38,3 +39,36 @@ class FileLogger(Logger):
         """
         self._log_count += 1
         self.logs.append(message)
+
+
+class TimedLogger(FileLogger):
+    def log_event(self, message: str) -> None:
+        """
+        Добавляет сообщение в список logs и временную метку.
+        А так же увеличивает счетчик лога _log_count.
+
+        Args:
+            message (str): Сообщение для логирования.
+        """
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Временная метка лога
+        timed_message: dict[str, str] = {"message": message, "created_at": timestamp}
+        super().log_event(timed_message)
+
+    def __getitem__(self, index: int):
+        """
+        Возвращает лог по индексу.
+
+        Args:
+            index (int): Индекс лога.
+
+        Returns:
+            dict[str, str]: Словарь логов.
+        """
+        return self.logs[index]
+
+
+loger = TimedLogger()
+loger.log_event("Добавил что-то")
+loger.log_event("Удалил что-то")
+print(loger.logs)
+print(loger[0])
